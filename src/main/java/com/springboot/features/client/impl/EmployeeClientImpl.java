32197d8b3e7fd595a060047e0b3e8256f8e00621
rsarakeri <rsarakeri99@gmail.com>
2026-05-13 01:17:39 +0530

    logging-in-spring-boot-applications_787

--- src/main/java/com/springboot/features/client/impl/EmployeeClientImpl.java
+++ src/main/java/com/springboot/features/client/impl/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.springboot.features.client.EmployeeClient;
 import com.springboot.features.dto.EmployeeDTO;
 import com.springboot.features.exception.ResourceNotFound;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,8 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
         ApiResponse<List<EmployeeDTO>> response = restClient.get()
@@ -27,6 +31,8 @@ public class EmployeeClientImpl implements EmployeeClient {
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
                 });
+        log.debug("Successfully retried employee details");
+        log.info("Retrieved employees list : {} {} {}", 465, response, 9);
         return response.getData();
     }
 
@@ -38,7 +44,7 @@ public class EmployeeClientImpl implements EmployeeClient {
                     .body(employeeDTO)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError, ((request, response1) -> {
-                        System.out.println(new String(response1.getBody().readAllBytes()));
+                        log.error(new String(response1.getBody().readAllBytes()));
                         new ResourceNotFound("could not create employee");
                     }))
                     .body(new ParameterizedTypeReference<>() {
@@ -46,6 +52,7 @@ public class EmployeeClientImpl implements EmployeeClient {
 
             return response.getData();
         } catch (Exception e) {
+            log.error("Exception occurred in create employee", e);
             throw new RuntimeException(e);
         }
     }
@@ -53,6 +60,7 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     @Override
     public Optional<EmployeeDTO> getEmployeeById(Long id) {
+        log.info("Trying to retrieve employee id : {}", id);
         ApiResponse<Optional<EmployeeDTO>> response = restClient.get()
                 .uri("/employee/{id}", id)
                 .retrieve()
--- src/test/java/com/springboot/features/FeaturesApplicationTests.java
+++ src/test/java/com/springboot/features/FeaturesApplicationTests.java
@@ -3,6 +3,7 @@ package com.springboot.features;
 import com.springboot.features.client.EmployeeClient;
 import com.springboot.features.client.impl.EmployeeClientImpl;
 import com.springboot.features.dto.EmployeeDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,9 @@ import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
 class FeaturesApplicationTests {
+
 
     @Autowired
     private EmployeeClient employeeClient;
@@ -28,26 +31,28 @@ class FeaturesApplicationTests {
     @Test
     @Order(2)
     void checkEmployeeClient() {
-        employeeClient.getAllEmployees().stream().forEach(e -> System.out.println(e.toString()));
+        employeeClient.getAllEmployees().forEach(e -> log.info(e.toString()));
     }
 
     @Test
     @Order(1)
     void getEmployeeById() {
-        System.out.println(employeeClient.getEmployeeById(2L));
+        log.info(employeeClient.getEmployeeById(2L).toString());
     }
 
     @Test
     @Order(0)
     void createEmployee() {
-        System.out.println(employeeClient.create(new EmployeeDTO(
+        log.error(employeeClient.create(new EmployeeDTO(
                 null,
 				"RAHUL",
                 "asd@gmail.com",
                 7,
                 "ADMIN",
                 LocalDate.now(),
-                Boolean.TRUE)));
+                Boolean.TRUE)).toString());
+
+
     }
 
 }
